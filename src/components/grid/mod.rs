51e6be2edc226f6174ccfc9b88aab1fe3b52bc2c
Simon Gail <simongail@t-online.de>
2026-09-18 11:28:28 +0200

    revert some things

--- src/components/grid/mod.rs
+++ src/components/grid/mod.rs
@@ -4,8 +4,6 @@ use ratatui::{
     widgets::{Block, Borders, Padding, Paragraph, Widget},
 };
 
-mod column;
-
 #[derive(Debug)]
 struct CellCoordinates {
     x_coordinate: usize,
@@ -32,26 +30,13 @@ pub struct Grid {
 impl Grid {
     pub fn new() -> Self {
         Self {
-            cols: 7,
-            rows: 24,
+            cols: 5,
+            rows: 10,
             selected_cell: CellCoordinates {
                 x_coordinate: 0,
                 y_coordinate: 0,
             },
         }
-    }
-
-    fn get_weekday_from_index(&self, weekday_idx: usize) -> String {
-        return match weekday_idx {
-            0 => String::from("Montag"),
-            1 => String::from("Dienstag"),
-            2 => String::from("Mittwoch"),
-            3 => String::from("Donnnerstag"),
-            4 => String::from("Freitag"),
-            5 => String::from("Samstag"),
-            6 => String::from("Sonntag"),
-            _ => String::from(""),
-        };
     }
 
     fn is_cell_selected(&self, cell_x: usize, cell_y: usize) -> bool {
@@ -77,34 +62,15 @@ impl Widget for Grid {
         let chunks = Layout::default()
             .direction(ratatui::layout::Direction::Horizontal)
             .constraints(col_constraints.clone())
-            .spacing(Spacing::Overlap(1))
             .split(inner_area);
 
         let row_constraints = (0..self.rows).map(|_| Constraint::Length(3));
-        let vertical = Layout::vertical(row_constraints).spacing(Spacing::Overlap(1));
+        let vertical = Layout::vertical(row_constraints);
 
         let mut selected_cell: Option<Rect> = None;
 
         for (col_index, chunk) in chunks.iter().enumerate() {
-            let column_layout = Layout::default()
-                .direction(ratatui::layout::Direction::Vertical)
-                .constraints([Constraint::Length(3), Constraint::Fill(1)])
-                .spacing(Spacing::Overlap(1));
-
-            let [title_area, content_area] = column_layout.areas(*chunk);
-
-            let centered_title_area = title_area.centered_vertically(Constraint::Length(1));
-            let col_title = self.get_weekday_from_index(col_index);
-            Paragraph::new(col_title)
-                .block(
-                    Block::default()
-                        .borders(Borders::NONE)
-                        .padding(Padding::horizontal(2)),
-                )
-                .alignment(Alignment::Right)
-                .render(centered_title_area, buf);
-
-            let column_cells = vertical.split(content_area);
+            let column_cells = vertical.split(*chunk);
 
             for (row_index, cell) in column_cells.iter().enumerate() {
                 let is_selected = self.is_cell_selected(col_index, row_index);
@@ -124,9 +90,9 @@ impl Widget for Grid {
             Block::new()
                 .borders(Borders::ALL)
                 .border_type(ratatui::widgets::BorderType::Thick)
-                .style(Style::new().light_green())
-                .bg(Color::DarkGray)
+                .border_style(Style::new().light_blue())
                 .merge_borders(ratatui::symbols::merge::MergeStrategy::Exact)
+                .bg(Color::DarkGray)
                 .render(selected_area, buf);
         }
     }
